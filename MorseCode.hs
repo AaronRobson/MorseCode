7276e1a51101815892d55700e9dd80184d8b90bf
Aaron Robson <ARobsonSE@hotmail.co.uk>
2014-12-11 00:28:18 +0000

    Mentioning my sources.
    Youtube videos.

--- MorseCode.hs
+++ MorseCode.hs
@@ -1,4 +1,15 @@
 module MorseCode where
 
+{-
+Based on the following resources:
+
+International Morse Code, Hand Sending
+https://www.youtube.com/watch?v=R-petiNdCIY
+
+Technique Of Hand Sending (1944)
+https://www.youtube.com/watch?v=nVkLr0GyJPI
+(2:55 showing spacing)
+-}
+
 main :: IO ()
 main = do putStrLn "Morse Code application"
